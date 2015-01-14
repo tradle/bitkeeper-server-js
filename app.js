0253ac422f9d1fef98773e1e86090dab5e8f44c7
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-01-14 10:23:04 -0500

    log on start

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ var server = require('./');
 var minimist = require('minimist');
 var fs = require('fs');
 var path = require('path');
-var debug = require('debug')('bitkeeper-server');
 var argv = minimist(process.argv);
 
 var configPath;
 if (argv.keeper)
   configPath = path.join(__dirname, argv.keeper);
 else
-  configPath = path.join(__dirname + 'node_modules/bitkeeper-js/conf/config.json');
+  configPath = path.join(__dirname, 'node_modules/bitkeeper-js/conf/config.json');
 
 var config = fs.readFileSync(configPath, { encoding: 'utf8' });
 config = JSON.parse(config);
@@ -27,13 +26,13 @@ var port = argv.port || require('./conf/config.json').port;
 
 var keeper = new Keeper(config);
 keeper.on('ready', function() {
-  debug('Bitkeeper is ready, starting server...');
+  console.log('Bitkeeper is ready, starting server...');
   start();
 });
 
 function start() {
   server.create(keeper, port, function() {
-    debug('Bitkeeper server running on port ' + port);
+    console.log('Bitkeeper server running on port ' + port);
   });
 }
 
